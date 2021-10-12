a4bbe1afa207becd3a0a437f3a289512b14b2885
wuxudongxd <wuxudongxd@gmail.com>
2021-10-12 19:43:01 +0800

    feat(global): add Babel to compile Typescript

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser as uglify } from "rollup-plugin-terser";
+import { DEFAULT_EXTENSIONS } from "@babel/core";
+import babel from "@rollup/plugin-babel";
+
+export default {
+  input: "src/main.ts",
+  output: {
+    file: "dist/bundle.cjs.js",
+    format: "cjs",
+    sourcemap: true,
+  },
+  plugins: [
+    resolve({
+      resolveOnly: [""]
+    }),
+    commonjs(),
+    uglify(),
+    babel({
+      extensions: [...DEFAULT_EXTENSIONS, ".ts", ".tsx"],
+      babelHelpers: "runtime",
+      exclude: ["node_modules/**", /@babel\/runtime/],
+    }),
+  ],
+};
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+export let myName: string = "beijing";
+export let age: number = 12;
+console.log(myName, age);
+
+import { prompt } from "enquirer";
+
+console.log(prompt);
+
+// const response = await prompt({
+//   type: "input",
+//   name: "username",
+//   message: "What is your username?",
+// });
+
+// console.log(response);
+
+export let a = new Promise(() => {});
